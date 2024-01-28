c1be44e84f143c13e7512bcbfd0eb3201010e05e
wiyadathi <wiyada.t@bu.ac.th>
2024-01-28 14:38:28 +0700

    Ball and Bridge

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -1,18 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BallScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Texture newTexture;
+
+    private void Update()
     {
-        
+        GetComponent<Rigidbody>().useGravity = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision other)
     {
-        
+        GetComponent<Renderer>().material.mainTexture = newTexture;
+        GetComponent<Rigidbody>().useGravity = true;
+        Destroy(other.gameObject);
     }
 }
