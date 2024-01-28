43ee2d0ca628e70092fa534f85543d771f218b06
wiyadathi <wiyada.t@bu.ac.th>
2024-01-28 23:02:20 +0700

    2nd scene finish

--- Assets/Scripts/AddForce.cs
+++ Assets/Scripts/AddForce.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class AddForce : MonoBehaviour
 {
     [SerializeField] private float force;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
